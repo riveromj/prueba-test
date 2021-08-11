6822f0235eb3b535fba7980b66afe6bc5779e3f7
riveromj <marigen@gmail.com>
2021-08-11 19:30:39 +0000

    commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hola");
+// this is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+module.exports = { sum };
